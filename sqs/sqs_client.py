02e9361591d265a9e21f999459334e15e2c5f1d6
Gabriel Rezende <gabrielcastrorezende@gmail.com>
2025-06-20 14:18:29 -0300

    ✍️ refactoring: Adaptando o uso da classe AWSConnection no sqs_client

--- sqs/sqs_client.py
+++ sqs/sqs_client.py
@@ -1,21 +1,12 @@
-import boto3
-import os
 import json
 from datetime import datetime
+from aws_services_common.aws_services import AWSConnection
 
-AWS_REGION = os.getenv('AWS_REGION')
-AWS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET = os.getenv('AWS_SECRET_ACCESS_KEY')
-
-client = boto3.resource(
-    'sqs', 
-    region_name = AWS_REGION,
-    aws_access_key_id = AWS_KEY_ID,
-    aws_secret_access_key = AWS_SECRET
-)
+aws_services = AWSConnection()
+sqs_client = aws_services.get_resource('sqs')
 
 def send_message(queue_name: str, message_body: dict):
-    queue = client.get_queue_by_name(QueueName=queue_name)
+    queue = sqs_client.get_queue_by_name(QueueName=queue_name)
     response = queue.send_message(MessageBody=message_body)
     return response
 
